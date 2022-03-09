067e24ad05673f702b7635b46fda07f516869348
Oladapo LAWORE <dapolawore@gmail.com>
2022-03-10 00:26:01 +0100

    Update contains integration tests

--- src/pricing-service/pricing.test.js
+++ src/pricing-service/pricing.test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('../app');
 
 describe('Compute Cost', () => {
-  test("It should response the GET method", async () => {
+  test("It should return the cost of the trip", async () => {
     const start = 'A'
     const end = 'E'
     const routeId = 1
@@ -11,10 +11,19 @@ describe('Compute Cost', () => {
     // expect(response.statusCode).toBe(200);
     // expect(response.statusCode).toBe(200);
   });
+  test("It should fail to compute trip for an invalid route", async () => {
+    const start = 'A'
+    const end = 'E'
+    const routeId = 100
+    const response = await request(app).get(`/pricing/pricingCost/${routeId}/${start}/${end}`);
+    expect(response.statusCode).toBe(400);
+    // expect(response.statusCode).toBe(200);
+    // expect(response.statusCode).toBe(200);
+  });
 })
 
 describe('Set Route Strategy', () => {
-  test("It should response the POST method", async () => {
+  test("It updated the route configuration", async () => {
     const routeId = 1
     const response = await request(app)
     .post(`/pricing/configureRouteStrategy/${routeId}`)
